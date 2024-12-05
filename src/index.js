432cafdae2da7a2c18879564c532a0c271821dcc
alaun <alexis.launay@etud.univ-angers.fr>
2024-12-05 21:26:47 +0100

    ajout du bateau qui bouge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 (function() {
     'use strict';
 
-    const menuHandicapButton = document.querySelector("#menu_handicap");
+    let etat = 1;
+    let position_top = ["200px","275px","20px","-250px"];
+    let position_left = ["200px","700px","1150px","850px"];
 
-    menuHandicapButton.addEventListener("click", function(){
+    const bateau = document.querySelector("#bateau");
 
-    });
+    bateau.style.position = "absolute";
+    bateau.style.zIndex = "-1";
+    bateau.style.top = position_top[0];
+    bateau.style.left = position_left[0];
+    
+
+    setInterval(function(){
+        if (etat > 3) etat = 0;
+        bateau.style.top = position_top[etat];
+        bateau.style.left = position_left[etat];
+        etat++;
+    },1000);
 
 })();
