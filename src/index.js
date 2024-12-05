c7c5bbaddb4b29d57e0cf30b2baba4d7f14e7051
alaun <alexis.launay@etud.univ-angers.fr>
2024-12-05 19:43:26 +0100

    ajout index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    const menuHandicapButton = document.querySelector("#menu_handicap");
+
+    menuHandicapButton.addEventListener("click", function(){
+
+    });
+
+})();
